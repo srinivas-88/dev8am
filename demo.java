793c8fd9ab4aa026bc2f9ff7795d41fd4e1eb31a
root <root@ip-172-31-40-149.ap-south-1.compute.internal>
2023-01-19 10:29:05 +0000

    new code

---
+++ demo.java
@@ -0,0 +1,26 @@
+class demo 
+{
+public static void main(....)
+{
+
+
+
+s.o.print("test code");
+s.o.print("test code");
+
+s.o.print("test code");
+
+s.o.print("test code");
+
+
+
+
+}
+
+
+
+
+
+
+}
+
