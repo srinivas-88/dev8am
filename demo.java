897564ff193da68485c12a1eb0579450fecff19e
root <root@ip-172-31-40-149.ap-south-1.compute.internal>
2023-01-22 13:29:09 +0000

    arith ops

---
+++ demo.java
@@ -0,0 +1,28 @@
+void add()
+{
+c=a+b
+
+}
+void sub()
+{
+
+c=a-b	
+
+
+}
+class demo
+{
+
+  public static void main(....)
+  {
+	  s.o.print("test code");
+	  s.o.print("test code");
+	  s.o.print("test code");
+	  s.o.print("test code");
+
+
+  }
+
+
+
+}
